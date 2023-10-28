52506ed695a64d0e25084b0fbfa04150314bc8b2
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-10-28 12:46:31 +0530

    spiral queston

--- spiralMatrix.java
+++ spiralMatrix.java
@@ -1,5 +1,13 @@
 public class spiralMatrix {
 
+    public static void printNormally(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+
     public static void printSpiral(int matrix[][]) {
         int startRow = 0;
         int startCol = 0;
@@ -8,19 +16,46 @@ public class spiralMatrix {
 
         while (startRow <= endRow && startCol <= endCol) {
             // top
-            for (int j = startCol + 1; i <= endCol; i++) {
-                System.out.println(matrix[startRow][j]);
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
             }
 
             // right
             for (int i = startRow + 1; i <= endRow; i++) {
-                System.out.println(matrix[i][endCol]);
+                System.out.print(matrix[i][endCol] + " ");
             }
+
+            // bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            // left
+            for (int i = endRow - 1; i >= startRow + 1; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
         }
     }
 
     public static void main(String[] args) {
-        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int matrix[][] = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
         printSpiral(matrix);
+        // printNormally(matrix);
+
     }
 }
